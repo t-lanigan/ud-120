8bf9ca1614d153fd3552139f08bee19def9caf1e
Tyler Lanigan <tyler@Eva.local>
2016-09-21 12:31:00 -0700

    added svm exercises

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,38 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+import numpy as np
 
+def prettyPicture(clf, X_test, y_test):
+    x_min = 0.0; x_max = 1.0
+    y_min = 0.0; y_max = 1.0
+    
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    h = .01  # step size in the mesh
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    plt.pcolormesh(xx, yy, Z, cmap=pl.cm.seismic)
+
+    # Plot also the test points
+    grade_sig = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    bumpy_sig = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    grade_bkg = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==1]
+    bumpy_bkg = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==1]
+
+    plt.scatter(grade_sig, bumpy_sig, color = "b", label="fast")
+    plt.scatter(grade_bkg, bumpy_bkg, color = "r", label="slow")
+    plt.legend()
+    plt.xlabel("bumpiness")
+    plt.ylabel("grade")
+
+    plt.savefig("test.png")
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,11 +51,27 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+#reduce size to increase speed
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+### create classifier
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10000)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print pred[10]
+print pred[26]
+print pred[50]
+
+#prettyPicture(clf,features_test,labels_test)
+
+### use the trained classifier to predict labels for the test features
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print "accuracy: ", acc
 
 
-#########################################################
-### your code goes here ###
-
-#########################################################
 
 
