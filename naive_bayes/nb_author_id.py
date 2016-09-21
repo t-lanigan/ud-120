a67f679f866438fb8d07293e35f0c422d2f25f00
Tyler Lanigan <tyler@Eva.local>
2016-09-21 11:30:33 -0700

    added nb classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,20 +14,29 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
 
+### create classifier
+clf = GaussianNB()
+
+### fit the classifier on the training features and labels
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+### use the trained classifier to predict labels for the test features
+pred = clf.predict(features_test)
 
 
+from sklearn.metrics import accuracy_score
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
+print accuracy_score(pred,labels_test)
 
 
