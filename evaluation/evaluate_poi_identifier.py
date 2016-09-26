4fa189e7ec4fa7f6a40a10b2012689d602ec01e7
Tyler Lanigan <tyler@Eva.local>
2016-09-26 11:18:38 -0700

    added evaluation files

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,6 +15,7 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from time import time
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -25,7 +26,34 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### your code goes here 
+### it's all yours from here forward!  
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+clf = tree.DecisionTreeClassifier()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
 
 
+print "predicting time:", round(time()-t1, 3), "s"
+
+print "precision_score"
+print precision_score(	labels_test,pred)
+
+print "recall_score"
+print recall_score(labels_test,pred)
+
+print "confusion_matrix: "
+print confusion_matrix(labels_test,pred)
+
+print accuracy_score(pred,labels_test)
